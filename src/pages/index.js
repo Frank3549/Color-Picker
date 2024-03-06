ede12115f225fd480ebd51896b0c4f8daed75461
Frank3549 <fbautista@middlebury.edu>
2024-03-06 12:50:12 -0800

    done and working

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,12 +38,32 @@ export default function ColorPicker() {
   const [red, setRed] = useState(0);
   const [green, setGreen] = useState(0);
   const [blue, setBlue] = useState(0);
+  const [name, setName] = useState("");
 
+  const updateNearest = () => {
+    setRed(parseInt(name.closest_named_hex.substring(1, 3), 16));
+    setGreen(parseInt(name.closest_named_hex.substring(3, 5), 16));
+    setBlue(parseInt(name.closest_named_hex.substring(5, 7), 16));
+  };
+
+  useEffect(() => {
+    const queryTimeout = setTimeout(async () => {
+      const response = await fetch(
+        `https://www.thecolorapi.com/id?rgb=(${red},${green},${blue})`,
+      );
+      if (response.ok) {
+        const colorInfo = await response.json();
+        setName(colorInfo.name);
+      }
+    }, 250);
+    return () => {
+      clearTimeout(queryTimeout);
+    };
+  }, [red, green, blue]);
 
   const color = {
     background: `rgb(${red}, ${green}, ${blue})`,
   };
-
 
   return (
     <>
@@ -63,6 +83,20 @@ export default function ColorPicker() {
           <LabeledSlider label="Red" value={red} setValue={setRed} />
           <LabeledSlider label="Green" value={green} setValue={setGreen} />
           <LabeledSlider label="Blue" value={blue} setValue={setBlue} />
+          {name && (
+            <div>
+              <p>
+                Most similar named color: {name.value}{" "}
+                {name.exact_match_name ? (
+                  "🎯"
+                ) : (
+                  <button onClick={updateNearest} type="button">
+                    Switch
+                  </button>
+                )}
+              </p>
+            </div>
+          )}
         </div>
       </main>
     </>
